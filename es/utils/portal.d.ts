a55442ba6309a1baf6dfb2edd65078b30f25765a
shenxuxiang <shenxuxiang@ahqianmo.com>
2023-10-27 17:24:56 +0800

    feat: 修改

--- es/utils/portal.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-type ReactPortalProps = {
-    className?: string;
-    children: React.ReactNode;
-};
-declare function ReactPortal(props: ReactPortalProps): React.ReactPortal;
-declare const _default: React.MemoExoticComponent<typeof ReactPortal>;
-export default _default;
